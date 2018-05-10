caa3d9e874431863982d5469ea73e34dd7260837
Mustfa Rumeli <mustafarumeli@outlook.com>
2018-05-11 00:41:12 +0300

    generic

--- main.c
+++ main.c
@@ -2,21 +2,23 @@
 #include <gsl\gsl_linalg.h>
 #include <string.h>
 #include <stdlib.h>
-#define MAX 863
-#define SUPERMAX 744769
-#define CLUSTERCOUNT 45
-#define SUPERCLUSTERCOUNT (CLUSTERCOUNT * 2 + 1)
+#define MAX 20000
+#define SUPERMAX MAX*MAX
+#define CLUSTERCOUNT 10
+#define OUTPUTCOUNT 2
+#define CLUOUTCOUNT (CLUSTERCOUNT * OUTPUTCOUNT)
+#define SUPERCLUSTERCOUNT (CLUOUTCOUNT + 1)
+#define INPUTDIMENSIONSIZE 14
+
 #include <math.h>
 
 typedef struct Node //We constructed a struct for each point  and center 
 {
-	double X;
-	double Y;
+	double* Points;
 } Node;
-typedef struct Centroid //For differ centroid 
+typedef struct Centroid //For differ centroids from nodes 
 {
-	double X;
-	double Y;
+	double* Points;
 } Centroid;
 
 double** initiateMatrix(int x, int y); //For simplifing initiate matrices we created this function 
@@ -41,15 +43,112 @@ double** RbfTrainer(Node xTrain[MAX], int yTrain[MAX], int centersPerCat, Centro
 double GetEuclideanDistanceSquared(Node node, Centroid center); // for calculating EuclideanDistance between given node and centroid
 double GetGaussianResult(double beta, Centroid center, Node node); // for calculating GaussianResult
 Centroid* PrepareInitialCentroids(Node* nodes); // for PreparingInitialCentroids for kmeans and rbf
+double SumMatrix(double* distances, int size);
 
+int main()
+{
+
+	char buffer[1024];
+	char *record, *line;
+	int i = 0;
+	Node nodes[MAX];
+	int  classify[MAX];
+	FILE *fstream = fopen("C:\\Users\\zuzu\\source\\repos\\Project3\\datasetNew.csv", "r");
+	if (fstream == NULL)
+	{
+		printf("\n file opening failed ");
+		return -1;
+	}
+	while ((line = fgets(buffer, sizeof(buffer), fstream)) != NULL && i <= MAX)
+	{
+		record = strtok(line, ",");
+		nodes[i].Points = malloc(sizeof(double) * INPUTDIMENSIONSIZE);
+		int c = 0;
+		//printf("%d\n", i);    //here you can put the record into the array as per your requirement.
+		while (record != NULL)
+		{
+
+			if (c < INPUTDIMENSIONSIZE)
+			{
+				nodes[i].Points[c] = atof(record);			
+			}
+			else
+			{
+				classify[i] = atoi(record);
+			}
+			record = strtok(NULL, ",");
+			c++;
+		}
+		i++;
+		if (i == MAX)
+		{
+			break;
+		}
+
+	}
+	double* betas = malloc(CLUOUTCOUNT * sizeof(double));
+	Centroid* centers = malloc(CLUOUTCOUNT * sizeof(Centroid));
+	double** theta = RbfTrainer(nodes, classify, CLUSTERCOUNT, centers, betas);
+	int correctCount = 0;
+	Node ErrorNodes[MAX];// = malloc(sizeof(Node) * MAX);
+	int errorCounter = 0;
+	for (size_t i = 0; i < MAX; i++)
+	{
+		double* scores = EvaluateRBFN(centers, betas, theta, nodes[i]);
+		if (scores[0] > scores[1])
+		{
+			if (classify[i] == 1)
+			{
+				correctCount++;
+			}
+			else
+			{
+				ErrorNodes[errorCounter++] = nodes[i];
+			}
+		}
+		else
+		{
+			if (classify[i] == 2)
+			{
+				correctCount++;
+			}
+			else
+			{
+				ErrorNodes[errorCounter++] = nodes[i];
+			}
+		}
+
+	}
+	double acc = ((double)correctCount / MAX);
+	printf("ACC: %f", acc * 100.0);
+	getch();
+	return 0;
+}
 
 double GetEuclideanDistanceSquared(Node node, Centroid center) {
-	double xDistance = node.X - center.X;
-	xDistance *= xDistance;
-	double yDistance = node.Y - center.Y;
-	yDistance *= yDistance;
-	return xDistance + yDistance;
+	double* distances = malloc(sizeof(double) * INPUTDIMENSIONSIZE);
+	for (size_t i = 0; i < INPUTDIMENSIONSIZE; i++)
+	{
+		distances[i] = node.Points[i] - center.Points[i];
+		distances[i] *= distances[i];
+	}
+
+	return SumMatrix(distances, INPUTDIMENSIONSIZE);
+	//double xDistance = node.X - center.X;
+	//xDistance *= xDistance;
+	//double yDistance = node.Y - center.Y;
+	//yDistance *= yDistance;
+	//return xDistance + yDistance;
 }
+double SumMatrix(double* distances,int size) {
+	double total = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		total += distances[i];
+	}
+	return total;
+}
+
 double GetGaussianResult(double beta, Centroid center, Node node) {
 	return exp(-1 * beta*GetEuclideanDistanceSquared(node, center));
 }
@@ -57,6 +156,11 @@ Centroid* Kmeans(Node* X, int xDataSize, Centroid* initial_centroids, int max_it
 	int k = CLUSTERCOUNT;
 	Centroid* centroids = malloc(k * sizeof(Centroid));
 	Centroid* prevCentroids = malloc(k * sizeof(Centroid));
+	for (size_t i = 0; i < k; i++)
+	{
+		centroids[i].Points = malloc(sizeof(double) * INPUTDIMENSIONSIZE);
+		prevCentroids[i].Points = malloc(sizeof(double) * INPUTDIMENSIONSIZE);
+	}
 	MakeTwoCentroidsEqual(initial_centroids, centroids, k);
 	MakeTwoCentroidsEqual(centroids, prevCentroids, k);
 
@@ -64,17 +168,7 @@ Centroid* Kmeans(Node* X, int xDataSize, Centroid* initial_centroids, int max_it
 	{
 		int* membership = malloc(MAX * sizeof(int));
 		membership = findClosestCentroids(X, xDataSize, centroids, CLUSTERCOUNT);
-		Centroid sa[CLUSTERCOUNT];
-		for (size_t i = 0; i < CLUSTERCOUNT; i++)
-		{
-			sa[i] = centroids[i];
-		}
 		centroids = ComputeCentroids(X, xDataSize, centroids, membership, CLUSTERCOUNT);
-		Centroid sa2[CLUSTERCOUNT];
-		for (size_t i = 0; i < CLUSTERCOUNT; i++)
-		{
-			sa2[i] = centroids[i];
-		}
 		if (CheckIfCentroidsAreEqual(centroids, prevCentroids, CLUSTERCOUNT) == 1)
 		{
 			break;
@@ -88,17 +182,25 @@ Centroid* Kmeans(Node* X, int xDataSize, Centroid* initial_centroids, int max_it
 void MakeTwoCentroidsEqual(Centroid* first, Centroid* second, int size) {
 	for (size_t i = 0; i < size; i++)
 	{
-		second[i].X = first[i].X;
-		second[i].Y = first[i].Y;
+		for (size_t j = 0; j < INPUTDIMENSIONSIZE; j++)
+		{
+			second[i].Points[j] = first[i].Points[j];
+		}
+	
+	/*	second[i].X = first[i].X;
+		second[i].Y = first[i].Y;*/
 	}
 }
 int CheckIfCentroidsAreEqual(Centroid* first, Centroid* second, int size) {
 	for (size_t i = 0; i < size; i++)
 	{
-		if (first[i].X != second[i].X || first[i].Y != second[i].Y)
+		for (size_t j = 0; j < INPUTDIMENSIONSIZE; j++)
 		{
-			return 0;
-		}
+			if (first[i].Points[j] != second[i].Points[j])
+			{
+				return 0;
+			}
+		}		
 	}
 	return 1;
 }
@@ -106,16 +208,21 @@ Centroid* ComputeCentroids(Node* X, int xDataSize, Centroid* prevCentroids, int*
 	Centroid* centroid = malloc(CLUSTERCOUNT * sizeof(Centroid));
 	for (size_t i = 0; i < CLUSTERCOUNT; i++)
 	{
-		centroid[i].X = 0;
-		centroid[i].Y = 0;
+		centroid[i].Points = malloc(sizeof(double)*INPUTDIMENSIONSIZE);
+		for (size_t j = 0; j < INPUTDIMENSIONSIZE; j++)
+		{
+			centroid[i].Points[j] = 0;
+		}
 	}
 
 	for (size_t i = 0; i < k; i++)
 	{
 		if (HasElement(memberships, xDataSize, i) == 0)
 		{
-			centroid[i].X = prevCentroids[i].X;
-			centroid[i].Y = prevCentroids[i].Y;
+			for (size_t j = 0; j < INPUTDIMENSIONSIZE; j++)
+			{
+				centroid[i].Points[j] = prevCentroids[i].Points[j];
+			}
 		}
 		else
 		{
@@ -125,14 +232,19 @@ Centroid* ComputeCentroids(Node* X, int xDataSize, Centroid* prevCentroids, int*
 			{
 				if (memberships[t] == i)
 				{
-					points[pointCounter].X = X[t].X;
-					points[pointCounter].Y = X[t].Y;
+					points[pointCounter].Points = malloc(sizeof(double)*INPUTDIMENSIONSIZE);
+					for (size_t j = 0; j < INPUTDIMENSIONSIZE; j++)
+					{
+						points[pointCounter].Points[j] = X[t].Points[j];
+					}
 					pointCounter++;
 				}
 			}
 			Centroid cNow = FindMeanForNodes(points, pointCounter);
-			centroid[i].X = cNow.X;
-			centroid[i].Y = cNow.Y;
+			for (size_t j = 0; j < INPUTDIMENSIONSIZE; j++)
+			{
+				centroid[i].Points[j] = cNow.Points[j];
+			}
 		}
 	}
 	return centroid;
@@ -162,15 +274,24 @@ int* findClosestCentroids(Node* data, int m, Centroid* centroids, int k) {
 	return membership;
 }
 Centroid FindMeanForNodes(Node* nodes, int nodeSize) {
-	double xSum = 0, ySum = 0;
+	double* sums = malloc(sizeof(double) * INPUTDIMENSIONSIZE);
+	for (size_t i = 0; i < INPUTDIMENSIONSIZE; i++)
+	{
+		sums[i] = 0;
+	}
 	for (size_t i = 0; i < nodeSize; i++)
 	{
-		xSum += nodes[i].X;
-		ySum += nodes[i].Y;
+		for (size_t j = 0; j < INPUTDIMENSIONSIZE; j++)
+		{
+			sums[j] += nodes[i].Points[j];
+		}
 	}
 	Centroid c;
-	c.X = (xSum / (double)nodeSize);
-	c.Y = (ySum / (double)nodeSize);
+	c.Points = malloc(sizeof(double) * INPUTDIMENSIONSIZE);
+	for (size_t j = 0;j < INPUTDIMENSIONSIZE; j++)
+	{
+		c.Points[j] = sums[j] / (double) nodeSize;
+	}
 	return c;
 }
 int FindMin(double arry[MAX], int size) {
@@ -191,77 +312,64 @@ Centroid* PrepareInitialCentroids(Node* nodes)
 	Centroid* init_centroids = malloc(CLUSTERCOUNT * sizeof(Centroid));
 	for (size_t i = 0; i < CLUSTERCOUNT; i++)
 	{
-		init_centroids[i].X = nodes[i].X;
-		init_centroids[i].Y = nodes[i].Y;
+		init_centroids[i].Points = malloc(sizeof(double) * INPUTDIMENSIONSIZE);
+		for (size_t c = 0; c < INPUTDIMENSIONSIZE; c++)
+		{
+			init_centroids[i].Points[c] = nodes[i].Points[c];
+		}
 	}
 	return init_centroids;
 }
 double** RbfTrainer(Node xTrain[MAX], int yTrain[MAX], int centersPerCat, Centroid*  centerFull, double* betaFull) {
-	int* ycFull = malloc(MAX * sizeof(int));
+	double** ycFull = initiateMatrix(OUTPUTCOUNT, MAX);
 
-	int superCounter = 0;
-	int numberOfCats = GetDistinct(yTrain, MAX);
-	Node* XC1 = malloc(MAX * sizeof(Node));
-	int xc1Counter = 0, xc2Counter = 0;
-	Node* XC2 = malloc(MAX * sizeof(Node));
-	double** yc1 = initiateMatrix(MAX, 1);
-	double** yc2 = initiateMatrix(MAX, 1);
-
-	for (size_t j = 0; j < MAX; j++)
+	int lastplace = 0;
+	for (size_t i = 0; i < OUTPUTCOUNT; i++)
 	{
-		if (yTrain[j] == 1)
+		Node* XC = malloc(MAX * sizeof(Node));
+		int xcCounter = 0;
+		for (size_t j = 0; j < MAX; j++)
 		{
-			XC1[xc1Counter] = xTrain[j];
-			yc1[j][0] = 1;
-			yc2[j][0] = 0;
-			xc1Counter++;
+			if (yTrain[j] == i)
+			{				
+				XC[xcCounter] = xTrain[j];
+				xcCounter++;
+				ycFull[i][j] = 1;
+			}
+			else
+			{
+				ycFull[i][j] = 0;
+			}
 		}
-		else
+
+		Centroid* centro = PrepareInitialCentroids(XC);
+		Centroid* newCentro = Kmeans(XC, xcCounter, centro, 100);
+		int* membership = malloc(MAX * sizeof(int));
+		membership = findClosestCentroids(XC, xcCounter, newCentro, CLUSTERCOUNT);
+		double* betas = malloc(sizeof(double) * CLUSTERCOUNT);
+		betas = ComputeRBFBetas(XC, xcCounter, newCentro, membership);
+
+		for (size_t k = lastplace; k < CLUSTERCOUNT + lastplace; k++)
 		{
-			XC2[xc2Counter] = xTrain[j];
-			yc1[j][0] = 0;
-			yc2[j][0]= 1;
-			xc2Counter++;
+			betaFull[k] = betas[k - lastplace];
+			centerFull[k] = newCentro[k - lastplace];
 		}
+
+		lastplace += CLUSTERCOUNT;
 	}
 
-	Centroid* centro = PrepareInitialCentroids(XC1);// = (Centroid*)malloc(10 * sizeof(Centroid));
-	Centroid* newCentro = Kmeans(XC1, xc1Counter, centro, 100);
-	int* membership = malloc(MAX * sizeof(int));
-	membership = findClosestCentroids(XC1, xc1Counter, newCentro, CLUSTERCOUNT);
-	double* betas = malloc(sizeof(double) * CLUSTERCOUNT);
-	betas = ComputeRBFBetas(XC1, xc1Counter, newCentro, membership);
 
-	Centroid* centro2 = PrepareInitialCentroids(XC2);// = (Centroid*)malloc(10 * sizeof(Centroid));
-	Centroid* newCentro2 = Kmeans(XC2, xc2Counter, centro2, 100);
-	int* membership2 = malloc(MAX * sizeof(int));
-	membership2 = findClosestCentroids(XC2, xc2Counter, newCentro2, CLUSTERCOUNT);
-	double* betas2 = malloc(sizeof(double) * CLUSTERCOUNT);
-	betas2 = ComputeRBFBetas(XC2, xc2Counter, newCentro2, membership2);
-
-
-	for (size_t k = 0; k < CLUSTERCOUNT; k++)
-	{
-		betaFull[k] = betas[k];
-		betaFull[k + CLUSTERCOUNT] = betas2[k];
-		centerFull[k] = newCentro[k];
-		centerFull[k + CLUSTERCOUNT] = newCentro2[k];
-	}
-	for (size_t i = 0; i < xc1Counter; ycFull[i] = yc1[i++]);
-	for (size_t i = xc2Counter, j = 0; i < MAX; ycFull[i++] = yc2[j++]);
-
-	//double** x_active = initiateMatrix(MAX, 21);
 	double** x_active = initiateMatrix(MAX, SUPERCLUSTERCOUNT);
 	for (size_t counter = 0; counter < MAX; counter++)
 	{
-		double* z = malloc(sizeof(double) * CLUSTERCOUNT * 2);
+		double* z = malloc(sizeof(double) * CLUOUTCOUNT);
 		z = GetRBFActivations(centerFull, betaFull, xTrain[counter]);
-		double* zTranspose = GetTransposeArray(z, CLUSTERCOUNT * 2);
-		for (size_t i = 0; i < CLUSTERCOUNT * 2; i++)
+		double* zTranspose = GetTransposeArray(z, CLUOUTCOUNT);
+		for (size_t i = 0; i < CLUOUTCOUNT; i++)
 		{
 			x_active[counter][i] = 1;
 		}
-		for (size_t i = 0; i < CLUSTERCOUNT * 2; i++)
+		for (size_t i = 0; i < CLUOUTCOUNT; i++)
 		{
 			x_active[counter][i + 1] = z[i];
 		}
@@ -270,17 +378,22 @@ double** RbfTrainer(Node xTrain[MAX], int yTrain[MAX], int centersPerCat, Centro
 	double** transposed = GetTranspose(x_active, MAX, SUPERCLUSTERCOUNT);
 	double** multiplied = MatrixMultiply(transposed, x_active, SUPERCLUSTERCOUNT, SUPERCLUSTERCOUNT, MAX);
 	double** pinv = inverse(multiplied, SUPERCLUSTERCOUNT);
-	double** pinbS = MatrixMultiply(pinv, transposed,SUPERCLUSTERCOUNT,MAX,SUPERCLUSTERCOUNT);
-	
-	double** theta1 = MatrixMultiply(pinbS, yc1, SUPERCLUSTERCOUNT, 1, MAX);
-	double** theta2 = MatrixMultiply(pinbS, yc2, SUPERCLUSTERCOUNT, 1, MAX);
-	double** theta = initiateMatrix(SUPERCLUSTERCOUNT, 2);
-	for (size_t i = 0; i < SUPERCLUSTERCOUNT; i++)
-	{
-		theta[i][0] = theta1[i][0];
-		theta[i][1] = theta2[i][0];
-	}
+	double** pinbS = MatrixMultiply(pinv, transposed, SUPERCLUSTERCOUNT, MAX, SUPERCLUSTERCOUNT);
 
+	double** theta = initiateMatrix(SUPERCLUSTERCOUNT, OUTPUTCOUNT);
+	for (size_t i = 0; i < OUTPUTCOUNT; i++)
+	{
+		double ** ycTemp = initiateMatrix(MAX, 1);
+		for (size_t k = 0; k < MAX; k++)
+		{
+			ycTemp[k][0] = ycFull[i][k];
+		}
+		double** thetaTemp = MatrixMultiply(pinbS, ycTemp, SUPERCLUSTERCOUNT, 1, MAX);
+		for (size_t j = 0; j < SUPERCLUSTERCOUNT; j++)
+		{
+			theta[j][i] = thetaTemp[j][0];
+		}
+	}
 	return theta;
 }
 double** initiateMatrix(int x, int y) {
@@ -328,7 +441,7 @@ double** inverse(double** a, int k) {
 	gsl_permutation_free(p);
 	return inversed;
 }
-double** MatrixMultiply(double** first, double** second, int m, int q,int np) {
+double** MatrixMultiply(double** first, double** second, int m, int q, int np) {
 
 	double** multiply = initiateMatrix(m, q);
 	double sum = 0;
@@ -407,123 +520,41 @@ int HasElement(int arry[MAX], int size, int val) {
 	}
 	return 0;
 }
-int main()
-{
-
-	char buffer[1024];
-	char *record, *line;
-	int i = 0;
-	Node nodes[MAX];
-	int  classify[MAX];
-	FILE *fstream = fopen("C:\\Users\\zuzu\\Desktop\\RBFN Example\\dataset.csv", "r");
-	if (fstream == NULL)
-	{
-		printf("\n file opening failed ");
-		return -1;
-	}
-	while ((line = fgets(buffer, sizeof(buffer), fstream)) != NULL && i <= MAX)
-	{
-		record = strtok(line, ",");
-		int c = 0;
-		//printf("%d\n", i);    //here you can put the record into the array as per your requirement.
-		while (record != NULL)
-		{
-
-			if (c < 2)
-			{
-				if (c == 0)
-				{
-					nodes[i].X = atof(record);
-				}
-				else
-				{
-					nodes[i].Y = atof(record);
-				}
-			}
-			else
-			{
-				classify[i] = atoi(record);
-			}
-			record = strtok(NULL, ",");
-			c++;
-		}
-		i++;
-		if (i == MAX)
-		{
-			break;
-		}
-	}
-	double* betas = malloc(CLUSTERCOUNT * 2 * sizeof(double));
-	Centroid* centers = malloc(CLUSTERCOUNT * 2 * sizeof(Centroid));
-	double** theta = RbfTrainer(nodes, classify, CLUSTERCOUNT, centers, betas);
-	int correctCount = 0;
-	Node ErrorNodes[MAX];// = malloc(sizeof(Node) * MAX);
-	int errorCounter = 0;
-	for (size_t i = 0; i < MAX; i++)
-	{
-		double* scores = EvaluateRBFN(centers, betas, theta, nodes[i]);
-		if (scores[0] > scores[1])
-		{
-			if (classify[i] == 1)
-			{
-				correctCount++;
-			}
-			else
-			{
-				ErrorNodes[errorCounter++] = nodes[i];
-			}
-		}
-		else
-		{
-			if (classify[i] == 2)
-			{
-				correctCount++;
-			}
-			else
-			{
-				ErrorNodes[errorCounter++] = nodes[i];
-			}
-		}
-
-	}
-	double acc = ((double)correctCount / MAX);
-	printf("ACC: %f", acc * 100.0);
-	getch();
-	return 0;
-}
 double* EvaluateRBFN(Centroid* centers, double* betas, double** theta, Node input) {
 	double* phisTemp = GetRBFActivations(centers, betas, input);
 	double** phis = initiateMatrix(SUPERCLUSTERCOUNT, 1);
-	phis[0][0]= 1;
+	phis[0][0] = 1;
 	for (size_t i = 1, phisTempCounter = 0; i < SUPERCLUSTERCOUNT; i++, phisTempCounter++)
 	{
 		phis[i][0] = phisTemp[phisTempCounter];
 	}
-	double** thetaTranspose = GetTranspose(theta, SUPERCLUSTERCOUNT, 2);
-	double** tempZ = MatrixMultiply(thetaTranspose, phis, 2, 1, SUPERCLUSTERCOUNT);
-	double z[2];
-	z[0] = tempZ[0][0];
-	z[1] = tempZ[1][0];
+	double** thetaTranspose = GetTranspose(theta, SUPERCLUSTERCOUNT, OUTPUTCOUNT);
+	double** tempZ = MatrixMultiply(thetaTranspose, phis, OUTPUTCOUNT, 1, SUPERCLUSTERCOUNT);
+	double z[OUTPUTCOUNT];
+	for (size_t i = 0; i < OUTPUTCOUNT; i++)
+	{
+		z[i] = tempZ[i][0];
+	}
 	return z;
 }
 double* GetRBFActivations(Centroid* centerFull, double* betaFull, Node currentNode) {
-	double* z = malloc(CLUSTERCOUNT * 2 * sizeof(double));
-	for (size_t i = 0; i < CLUSTERCOUNT * 2; i++)
+	double* z = malloc(CLUOUTCOUNT * sizeof(double));
+	for (size_t i = 0; i < CLUOUTCOUNT; i++)
 	{
 		z[i] = GetGaussianResult(betaFull[i], centerFull[i], currentNode);
 	}
 	return z;
 }
 double** GetTransposeArray(double* theta, int xCount) {
-	double** transposed = initiateMatrix(1,xCount);
+	double** transposed = initiateMatrix(1, xCount);
 	for (int i = 0; i < xCount; ++i)
 	{
 		transposed[0][i] = theta[i];
 	}
-		
+
 	return transposed;
 }
-double** GetTranspose(double** theta,int xCount,int yCount) {
+double** GetTranspose(double** theta, int xCount, int yCount) {
 	double** transposed = initiateMatrix(yCount, xCount);
 	for (int i = 0; i < xCount; ++i)
 		for (int j = 0; j < yCount; ++j)
